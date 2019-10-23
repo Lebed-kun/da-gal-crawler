e5ed07123805b216d716b4036a0af0ee2139951e
Lebed-kun <lebed-kun@mail.ru>
2019-10-23 16:30:32 +0400

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./frontend/index.js",
+  output: {
+    path: path.join(__dirname, "/static/"),
+    filename: "index_bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test : /\.scss|\.css$/,
+        use : ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
